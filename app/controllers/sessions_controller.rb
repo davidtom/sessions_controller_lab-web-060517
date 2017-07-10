95be4bd8d354ceffe06c8cdd5161ca1652e970c9
davidtom <davidjtomczyk@gmail.com>
2017-07-10 19:21:18 -0400

    complete lab

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    if !!params[:name] && params[:name] != ""
+      session[:name] = params[:name]
+      redirect_to "/"
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    render "sessions/new.html"
+  end
+
+end
